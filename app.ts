667ee22cb970e5478bd678d311353b54e69cf13d
AbayoHJovin <abayohirwajovin@gmail.com>
2025-05-19 10:38:14 +0200

    improved readme

--- app.ts
+++ app.ts
@@ -12,8 +12,8 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    // origin:"http://localhost:8080",
-    origin:"https://citizenvoice-gamma.vercel.app",
+    origin:"http://localhost:8080",
+    // origin:"https://citizenvoice-gamma.vercel.app",
     credentials: true,
   })
 );
